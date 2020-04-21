29757c6a76e4020a7b928aa4dbe8915ef952f53a
josuedp12 <josue9895@gmail.com>
2020-04-21 03:29:14 -0400

    Se agrego sidebar y se eliminó opcion

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="app">
+    <div class="dashboard-main-wrapper">
+      <navbar />
+      <sidebar />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import navbar from "@/components/navbar";
+import sidebar from "@/components/sidebar";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  components: { navbar, sidebar }
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
